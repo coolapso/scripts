1967b16cdffce65c804db9d88df43f01c4a3a65b
Carlos Colaço <coolapso@coolapso.tech>
2024-05-23 01:24:47 +0200

    Update gitconfig

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 ## Configures git
-key="C6D9BD369FCF4966641E3F79AC2D3B898F96BC51"
+key="F775AADED032998CCB3A555431A25E931748A8F9"
 
 case "$1" in
 "work")
-  git config user.name 'Carlos "4s3ti" Colaço'
+  git config user.name 'Carlos Colaço'
   git config user.email "$WORK_EMAIL"
   git config user.signingkey "$key"
   ;;
 *)
-  git config user.name 4s3ti
-  git config user.email 4s3ti@4s3ti.net
+  git config user.name 'Carlos Colaço'
+  git config user.email coolapso@coolapso.tech
   git config user.signingkey "$key"
   ;;
 esac
