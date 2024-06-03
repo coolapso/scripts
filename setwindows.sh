1933cd1908007a776fc21de9df5ff341ecd4e280
Carlos Colaço <coolapso@coolapso.tech>
2024-06-03 22:54:09 +0200

    stwindows: Fix behavior based on OS

--- setwindows.sh
+++ setwindows.sh
@@ -2,10 +2,16 @@
 source /etc/os-release
 swaymsg "[app_id=ferdium]" move to workspace 1, move left
 swaymsg "[app_id=Alacritty]" move to workspace 1, move right
-swaymsg "[app_id=google-chrome]" move to workspace 1, move right
+if [[ ${ID} == "fedora" ]]; then
+  swaymsg "[app_id=google-chrome]" move to workspace 1, move right
+else
+  swaymsg "[app_id=brave-browser]" move to workspace 1, move right
+fi
+
+## Set window sizes
 swaymsg "[app_id=ferdium]" resize set width 1250px
 if [[ ${ID} == "fedora"  ]]; then
   swaymsg "[app_id=google-chrome]" resize set width 1400px
 else 
-  swaymsg "[brave-browser]" resize set width 1400px
+  swaymsg "[app_id=brave-browser]" resize set width 1400px
 fi
