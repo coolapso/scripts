761920007250eb668f86a1f73aadd9ab424083a3
Carlos Colaço <coolapso@coolapso.tech>
2024-11-06 14:19:36 +0100

    allow gitconfig to set global config

--- gitconfig.sh
+++ gitconfig.sh
@@ -2,15 +2,21 @@
 ## Configures git
 key="F775AADED032998CCB3A555431A25E931748A8F9"
 
+gitargs=('--replace-all')
+if [[ $2 == "global" ]]; then
+  gitargs+=('--global')
+fi
+
+## Accepts a second argument for git, for example --global if want to configure git globally on the machine
 case "$1" in
 "work")
-  git config user.name 'Carlos Colaço'
-  git config user.email "$WORK_EMAIL"
-  git config user.signingkey "$key"
+  git config ${gitargs[*]} user.name 'Carlos Colaço'
+  git config ${gitargs[*]} user.email "$WORK_EMAIL"
+  git config ${gitargs[*]} user.signingkey "${key}"
   ;;
 *)
-  git config user.name 'Carlos Colaço'
-  git config user.email coolapso@coolapso.tech
-  git config user.signingkey "$key"
+  git config ${gitargs[*]} user.name 'Carlos Colaço'
+  git config ${gitargs[*]} user.email coolapso@coolapso.tech
+  git config ${gitargs[*]} user.signingkey "${key}"
   ;;
 esac
