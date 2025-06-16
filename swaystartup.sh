6bfaf48c1421ac617f6a5e00f5d5ed1b350ea23e
Carlos Colaço <coolapso@coolapso.tech>
2025-06-16 19:03:45 +0200

    start signal from script

--- swaystartup.sh
+++ swaystartup.sh
@@ -5,10 +5,12 @@ if [[ $ID == "fedora" ]]; then
   swaymsg "workspace 2"
   swaymsg "exec --no-startup-id vivaldi --profile-directory='Default'" && swaymsg "workspace 1"
   swaymsg "exec --no-startup-id vivaldi --profile-directory='Profile 3'"
+  swaymsg "exec --no-startup-id flatpak run org.signal.Signal --enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer --ozone-platform-hint=wayland"
 fi
 
 if [[ $ID == "arch" ]]; then
   swaymsg "exec --no-startup-id nextcloud"
   # exec --no-startup-id ferdium --UseOzonePlatform --ozone-platform-hint=wayland
+  swaymsg "exec  --no-startup-id 'signal-desktop --enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer --ozone-platform-hint=wayland"
   swaymsg "exec --no-startup-id vivaldi"
 fi
