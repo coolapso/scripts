32e601af355bec43abc4108d6fe8d001f557a7ef
Carlos Colaço <coolapso@coolapso.tech>
2025-06-16 23:43:04 +0200

    Start stuff and windows

--- setwindows.sh
+++ setwindows.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 source /etc/os-release
-swaymsg "[app_id=ferdium]" move to workspace 1, move left
-swaymsg "[app_id=Alacritty]" move to workspace 1, move right
-if [[ ${ID} == "fedora" ]]; then
-  swaymsg "[app_id=google-chrome]" move to workspace 1, move right
-else
-  swaymsg "[app_id=vivaldi-stable]" move to workspace 1, move right
-fi
 
-## Set window sizes
-swaymsg "[app_id=ferdium]" resize set width 1250px
-if [[ ${ID} == "fedora"  ]]; then
-  swaymsg "[app_id=google-chrome]" resize set width 1400px
-else 
-  swaymsg "[app_id=vivaldi-stable]" resize set width 1400px
+if swaymsg -t get_outputs -p | grep -q 'LG Electronics'; then
+  swaymsg "[title=tleft]" move to workspace 1, move left 9, resize set width 1060px
+  swaymsg "[title=dev]" move to workspace 1, resize set width 3000px
+  swaymsg "[title=tright]" move to workspace 1, move right 9, resize set width 1060px
+  swaymsg "[app_id=vivaldi-stable]" move to workspace 2
+  swaymsg "[title=tfloat]" move position center, resize set 3110px 1418px
+else
+  swaymsg "[title=tleft]" move to workspace 1
+  swaymsg "[title=dev]" move to workspace 2
+  swaymsg "[title=tright]" move to workspace 3
+  swaymsg "[app_id=vivaldi-stable]" move to workspace 4
+  swaymsg "[title=tfloat]" move position center, resize set 3000px 1418px
 fi
--- swaystartup.sh
+++ swaystartup.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 source /etc/os-release
 
+swaymsg "workspace 1"
+swaymsg "exec --no-startup-id alacritty -T tleft -e tmux new-session -A -s left"
+swaymsg "exec --no-startup-id alacritty -T dev"
+swaymsg "exec --no-startup-id alacritty -T tright -e tmux new-session -A -s right"
+swaymsg "exec --no-startup-id alacritty -T tfloat"
+
+swaymsg "exec --no-startup-id flatpak run com.core447.StreamController -b"
+swaymsg "exec --no-startup-id 'flatpak run com.slack.Slack -u'"
+swaymsg "exec --no-startup-id 'flatpak run com.discordapp.Discord --enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer --ozone-platform-hint=wayland --start-minimized'"
+
+
 if [[ $ID == "fedora" ]]; then
   swaymsg "workspace 2"
-  swaymsg "exec --no-startup-id vivaldi --profile-directory='Default'" && swaymsg "workspace 1"
+  swaymsg "exec --no-startup-id vivaldi --profile-directory='Default'"
+  swaymsg "workspace 3"
   swaymsg "exec --no-startup-id vivaldi --profile-directory='Profile 3'"
-  swaymsg "exec --no-startup-id flatpak run org.signal.Signal --enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer --ozone-platform-hint=wayland"
+  swaymsg "exec --no-startup-id flatpak run org.signal.Signal --enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer --ozone-platform-hint=wayland" 
+
 fi
 
 if [[ $ID == "arch" ]]; then
+  swaymsg "workspace 2"
   swaymsg "exec --no-startup-id vivaldi"
   swaymsg "exec --no-startup-id nextcloud"
   # exec --no-startup-id ferdium --UseOzonePlatform --ozone-platform-hint=wayland
   swaymsg "exec  --no-startup-id 'signal-desktop --enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer --ozone-platform-hint=wayland"
 fi
+
+sleep 0.5
+
+bash $HOME/scripts/setwindows.sh
+swaymsg "workspace 2"
